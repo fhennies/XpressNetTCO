a4be96665f3441927cc9612602897e01f57e85ee
Franz Hennies <fhennies@users.noreply.github.com>
2021-03-19 16:34:01 +0100

    cleaned up, better comments

--- src/config.h
+++ src/config.h
@@ -1,29 +1,53 @@
-#define MCP
-#define XPRESSNET
+// A simple TCO for XpressNet / XBus 
+// based on the XpressNetMaster library (Copyright (c) 08/2016 - 2020 Philipp Gahtow)
+// The code in this main program is published under the GPL
+// All definitions should go here
 
-#ifdef XPRESSNET
-	#ifdef MCP
-		#define PINSATMEL {2, 3, 4, 5, 6, 7, 8, 10, 11, 12, 13, 14, 15, 16, 17}
-	#else
-		#define PINSATMEL {2, 3, 4, 5, 6, 7, 8, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19}
-	#endif
-#endif
+// MCP23017 portexpander used or not - work in progress
+// choice not fully implemented in code
+#define MCP				
 
-// Adressen shifted by 1 for z21PG (6 sent here is 7 IRL)
+// Define XPressNet Client ID. Each module needs to have an individual ID different 
+// from any other device, including Multimaus etc
+// This overrides the definition in XpressNetMaster.h 0x5F
+#define MY_ADDRESS 0x5A		
+
+// Adresses are shifted by 1 for z21PG (6 sent from this code here is 7 in real life). 
+// By defining SHIFTADDR all later definitions can be real life addresses
 #define SHIFTADDR	1
 
+// If a MCP23017 portexpander is used, only the follwowing Atmega pins are available as I/Os. 
+// Pin 8 is used for the RS485 communication
+// 2, 3, 4, 5, 6, 7, 8, 10, 11, 12, 13, 14, 15, 16, 17
+// If no MPC is present, the I2C pins can additionally be used:
+// 18, 19
+
+// Atmega inputs (5 arrays in total, all must have same length!)
+// Define which pins of the Atmega are used as inputs (contact to GND for active)
+// D2-D8 (2-8); D10-D13(10-13); A0-A5 (14-19) (A4, A5 not if MCP)
 #define INATMEL			{3, 4, 5, 6, 7, 8, 10, 11, 12,  2, 14, 15, 16, 17}	//all inputs
-                                    //D2-D8 (2-8); D10-D13(10-13); A0-A5 (14-19) (A4, A5 not if MCP)
+// Define for each of the pins an adress and a position (left or right, "red" or "green" etc.)
 #define INATMELADD		{7, 7, 9, 9, 4, 4,  3,  3,  6,  6,  5,  5,  8,  8}	//Addr 1 for inputs
 #define INATMELPOS		{1, 0, 0, 1, 1, 0,  1,  0,  0,  1,  1,  0,  1,  0}	//Pos each Addr 1
+// Define for each of the pins a second adress and a position (left or right, "red" or "green" etc.)
+// With this setting, one switch can set two positions on different turnouts
 #define INATMELADD2	    {0, 0, 0, 0, 2, 2,  2,  2,  0,  0,  0,  0,  0,  0}	//Addr 2 for inputs, "0" if not in use
 #define INATMELPOS2	    {0, 0, 0, 0, 1, 1,  0,  0,  0,  0,  0,  0,  0,  0}	//Pos each Addr 2, "0" if not in use
 
+// Atmega outputs (3 arrays in total, all must have same length!)
+// Define which pins of the Atmega are used as outputs (the logic in the code sets HIGH as active
+// i.e. LEDs are conntected Output > Resistor > LED > GND)
+// D2-D8 (2-8); D10-D13(10-13); A0-A5 (14-19) (A4, A5 not if MCP)
 #define OUTATMEL		{}			//all outputs
+// Define for each of the pins an adress and a position (left or right, "red" or "green" etc.)
 #define OUTATMELADD	    {} 			//Addr for outputs
 #define OUTATMELPOS	    {}	    	//Pos each Addr
 
+// MCP23017 outputs (code supports only outputs on MCP23017) (3 arrays in total, all must have same length!)
+// Define which pins of the MCP23017 are used as outputs (the logic in the code sets HIGH as active
+// i.e. LEDs are conntected Output > Resistor > LED > GND)
+//0-7 port A; 8-15 port B
 #define OUTMCP			{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15}	//all outputs 
-                                    //0-7 port A; 8-15 port B
+// Define for each of the pins an adress and a position (left or right, "red" or "green" etc.)
 #define OUTMCPADD		{6, 6, 5, 5, 7, 7, 8, 8, 9, 9,  4,  4,  3,  3,  2,  2}	//Addr for outputs
 #define OUTMCPPOS		{0, 1, 1, 0, 1, 0, 1, 0, 1, 0,  0,  1,  0,  1,  0,  1}	//Pos each Addr
--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,18 @@
-#include <Arduino.h>
-#include <config.h>
-#include <XpressNetMaster.h>
-#include <Bounce2.h>
+// A simple TCO for XpressNet / XBus 
+// based on the XpressNetMaster library (Copyright (c) 08/2016 - 2020 Philipp Gahtow)
+// The code in this main program is published under the GPL
+// All definitions should be in config.h
 
-#ifdef MCP
-  #include <Wire.h>
-  #include <Adafruit_MCP23017.h>
+#include <Arduino.h>              // for platformio in Visual Studio Code
+#include <XpressNetMaster.h>      // XPressNet lib
+#include <Bounce2.h>              // Debounce Atmega inputs
+#include <config.h>               // All input / outputs definitioned in config.h
+#ifdef MCP                        // Choice not fully implemented  
+  #include <Wire.h>               // I2C library
+  #include <Adafruit_MCP23017.h>  // MCP23017 library
 #endif
 
-// #define MY_ADDRESS 0x5A
-
-XpressNetMasterClass XpressNet;
+XpressNetMasterClass XpressNet;   
 
 const byte shift = SHIFTADDR;
 
@@ -28,69 +30,67 @@ const byte MCPOut[] = OUTMCP;
 const uint16_t MCPOutAdd[] = OUTMCPADD;
 const byte MCPOutPos[] = OUTMCPPOS;
 
-Bounce * buttons = new Bounce[sizeof(MegaInp)];
+Bounce * buttons = new Bounce[sizeof(MegaInp)]; // one button obejct for each defined Atmega input
 
 #ifdef MCP
   Adafruit_MCP23017 mcp;
 #endif
 
-void notifyXNetTrnt(uint16_t Address, uint8_t data);
+void notifyXNetTrnt(uint16_t Address, uint8_t data);  //function defined at bottom, made known here
 
 void setup() {
-  XpressNet.setup(Loco128, 9);                        //Initialisierung XNet Serial und Send/Receive-PIN
-  mcp.begin();                                        // use default address 0
+  XpressNet.setup(Loco128, 9);                        // Initialisierung XNet Serial und Send/Receive-PIN
+  mcp.begin();                                        // Use default address 0
 
-  for (byte i = 0; i < sizeof(MegaInp); i++) {
-    buttons[i].attach(MegaInp[i], INPUT_PULLUP);      //setup the bounce instance for the current button
+  for (byte i = 0; i < sizeof(MegaInp); i++) {        // Loop through all defined inputs on Atmega
+    buttons[i].attach(MegaInp[i], INPUT_PULLUP);      // Setup the bounce instance for the current button
     buttons[i].interval(25);                          // interval in ms
   } 
 
-  for (byte i = 0; i < sizeof(MegaOut); i++) {
+  for (byte i = 0; i < sizeof(MegaOut); i++) {        // Loop through all defined outputs on Atmega
     pinMode(MegaOut[i], OUTPUT);
     digitalWrite(MegaOut[i], LOW);
   }
  
-  for (byte i = 0; i < sizeof(MCPOut); i++) {
-    mcp.pinMode(MCPOut[i], OUTPUT);
+  for (byte i = 0; i < sizeof(MCPOut); i++) {         // Loop through all defined outputs on MCP23017
+    mcp.pinMode(MCPOut[i], OUTPUT);                   // use MCP specific pin functions
     mcp.digitalWrite(MCPOut[i], LOW);
   }
 
 }
 
 void loop() {
-  XpressNet.update();	                                 //call in every loop
-  
+  // XPressNet communication routine, call in every loop
+  // Calls notifyXNetTrnt if there is any turnout notifocation send by master
+  XpressNet.update();	                                 
+
+// Loop through all Atmega input pins
   for (byte i = 0; i < sizeof(MegaInp); i++)  {
     buttons[i].update();                               // Update the Bounce instance
-    if (buttons[i].fell()) {
-      XpressNet.SetTrntPos(MegaInpAdd[i]-shift, MegaInpPos[i], 1);
-      notifyXNetTrnt(MegaInpAdd[i]-shift, (MegaInpPos[i] | 0x08));
-      if (MegaInpAdd2[i] != 0) {
-        XpressNet.update();	
-        delay(250);
+    if (buttons[i].fell()) {                           // Input changed to LOW (connected to ground)?
+      XpressNet.SetTrntPos(MegaInpAdd[i]-shift, MegaInpPos[i], 1);  // Send turnout command
+      notifyXNetTrnt(MegaInpAdd[i]-shift, (MegaInpPos[i] | 0x08));  // Call notifyXNetTrnt to update LED
+      if (MegaInpAdd2[i] != 0) {                        // If second address defiend for input, repeat above
+        XpressNet.update();	                            // Make sure first command is processed
+        delay(250);                                     // Give XPressNet some time (not sure if necessary)
         XpressNet.SetTrntPos(MegaInpAdd2[i]-shift, MegaInpPos2[i], 1);
         notifyXNetTrnt(MegaInpAdd2[i]-shift, (MegaInpPos2[i] | 0x08));
       }
     }
   }
+}                                                       // End of loop, outputs are handled in notifyXNetTrnt
 
-}
-
-void notifyXNetTrnt(uint16_t Address, uint8_t data) {
-  if (bitRead(data,3) == 0x01) {  //Weiche Aktiv == HIGH?
-    for (byte i = 0; i < sizeof(MegaOut); i++) {
-      if (Address == MegaOutAdd[i]-shift) {
-        digitalWrite(MegaOut[i], MegaOutPos[i]^(data & 0x01));
+void notifyXNetTrnt(uint16_t Address, uint8_t data) {   // Called by XpressNet.update in case 
+  if (bitRead(data,3) == 0x01) {                        // Weiche Aktiv == HIGH?
+    for (byte i = 0; i < sizeof(MegaOut); i++) {        // Loop through all Atmega outputs
+      if (Address == MegaOutAdd[i]-shift) {             // Output right address?
+        digitalWrite(MegaOut[i], MegaOutPos[i]^(data & 0x01));  // LED on/off dependend on pos 
       }
     }
-    for (byte i = 0; i < sizeof(MCPOut); i++) {
+    for (byte i = 0; i < sizeof(MCPOut); i++) {         // Loop through all MCP outputs
       if (Address == MCPOutAdd[i]-shift) {
         mcp.digitalWrite(MCPOut[i], MCPOutPos[i]^(data & 0x01));
       }
     }
-    // Serial.print("XNet TA:");
-    // Serial.print(Address);
-    // Serial.print(", P:");
-    // Serial.println(data & 0x01);
   }
 }
